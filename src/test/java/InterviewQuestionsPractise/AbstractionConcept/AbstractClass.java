54574424dc995fccaa7469ec0e738f3e677a9e45
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-03-31 02:05:41 +0530

    Practise on Abstract class understanding

---
+++ src/test/java/InterviewQuestionsPractise/AbstractionConcept/AbstractClass.java
@@ -0,0 +1,22 @@
+package InterviewQuestionsPractise.AbstractionConcept;
+
+/*
+ * Rules for abstract class:
+ * 1. "abstract" keyword should be used in class name
+ * 2. One method of the class has to be abstract method
+ * 2.1 Abstract method are method which don't have body, only method declaration
+ * 2.2 One method is mandatory, abstract class can have more than 1 abstract method but 1 is minimum required
+ * */
+
+public abstract class AbstractClass {
+
+    //This is how abstract method is defined, and are mandatory with abstract class.
+    public abstract void abstractMethod();
+
+    public abstract void anotherAbstractMethod();
+
+    //This is a non-abstract method in abstract class
+    public void concreteMethod() {
+        System.out.println("This is concrete method");
+    }
+}
---
+++ src/test/java/InterviewQuestionsPractise/AbstractionConcept/TestAbstractionClass.java
@@ -0,0 +1,26 @@
+package InterviewQuestionsPractise.AbstractionConcept;
+
+public class TestAbstractionClass {
+    public static void main(String[] args) {
+        AbstractClassInheritor abstractClassInheritor = new AbstractClassInheritor();
+        abstractClassInheritor.concreteMethod();
+        abstractClassInheritor.concreteMethodInInheritor();
+        abstractClassInheritor.abstractMethod(); //This is an overriden method from AbstractClass in AbstractClassInheritor class
+        abstractClassInheritor.anotherAbstractMethod(); //This is an overriden method from AbstractClass in AbstractClassInheritor class
+
+        //Dynamic Polymorphism
+        AbstractClass ab = new AbstractClassInheritor();
+        ab.abstractMethod();
+        ab.anotherAbstractMethod();
+        ab.concreteMethod();
+        /*
+        * Why this example was written because when calling abstract methods via dynamic polymorphism then it was called
+        * as overriden method from the child class.
+        *
+        * Note: AbstractClass or parent class is not able to call method of child class via "ab" because ab is reference of
+        * AbstractClass, though it is creating object with new keyword of AbstractClassInheritor but "ab" is reference
+        * of AbstractClass
+        * */
+
+    }
+}
