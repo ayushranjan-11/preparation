ac9503e0a2d211e9df4c44177e1f10d5903aba0c
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-04-06 22:51:55 +0530

    Broken links added

---
+++ src/test/java/SeleniumCodes/BrokenLinks.java
@@ -0,0 +1,58 @@
+package SeleniumCodes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrokenLinks {
+    static WebDriver driver = new ChromeDriver();
+    static String baseURL = "https://www.flipkart.com/";
+
+    public static void main(String[] args) {
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+        getLinks();
+    }
+
+    public static void getLinks() {
+
+        List<WebElement> linkList = driver.findElements(By.tagName("a"));
+        System.out.println(linkList.size());
+
+        List<String> urlList = new ArrayList<>();
+
+        for (WebElement link : linkList) {
+            //System.out.println(link.getDomAttribute("href"));
+            String linkHref = link.getDomProperty("href");
+            urlList.add(linkHref);
+            //linkVisit(linkHref);
+
+
+            urlList.parallelStream().forEach(linkList1 -> linkVisit(linkHref));
+
+        }
+    }
+
+    public static void linkVisit(String linkURL) {
+        try {
+            URL url = new URL(linkURL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(5000);
+            httpURLConnection.connect();
+
+            if (httpURLConnection.getResponseCode() >= 400) {
+                System.out.println(linkURL+ " Link is broken as response code is " + httpURLConnection.getResponseCode());
+            } else {
+                System.out.println(linkURL+ " Link is working as response code is " + httpURLConnection.getResponseCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
