ce13cf8ee7082ad62909146e8b8065a8b21c1e82
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-04-07 22:35:28 +0530

    Added string reverse in the javatopics.java class

--- src/test/java/InterviewQuestionsPractise/JavaTopics.java
+++ src/test/java/InterviewQuestionsPractise/JavaTopics.java
@@ -5,6 +5,8 @@ public class JavaTopics {
         System.out.println("Main method with main");
         main("Main method without main");
         main(100);
+        System.out.println(stringCompare("hello", "Hello"));
+        stringReverse("Hello");
     }
 
     public static void main(String string) {
@@ -14,4 +16,21 @@ public class JavaTopics {
     public static void main(int number) {
         System.out.println(number);
     }
+
+    public static boolean stringCompare(String a, String b) {
+        if (a == b) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void stringReverse(String providedString) {
+        String temporaryString = "";
+        for (int i = providedString.length() - 1; i >= 0; i--) {
+            temporaryString += providedString.charAt(i);
+        }
+        System.out.println("Reversed String: " + temporaryString);
+    }
+
 }
