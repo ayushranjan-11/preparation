129bc391ffb291db49b842bf86fcd6e398f0e9a8
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-04-09 12:49:30 +0530

    Added 2D matrix input and print method in javatopics.class

--- src/test/java/InterviewQuestionsPractise/JavaTopics.java
+++ src/test/java/InterviewQuestionsPractise/JavaTopics.java
@@ -1,12 +1,16 @@
 package InterviewQuestionsPractise;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class JavaTopics {
     public static void main(String[] args) {
-        System.out.println("Main method with main");
-        main("Main method without main");
-        main(100);
-        System.out.println(stringCompare("hello", "Hello"));
-        stringReverse("Hello");
+//        System.out.println("Main method with main");
+//        main("Main method without main");
+//        main(100);
+//        System.out.println(stringCompare("hello", "Hello"));
+//        stringReverse("Hello");
+        matrixInput();
     }
 
     public static void main(String string) {
@@ -18,7 +22,7 @@ public class JavaTopics {
     }
 
     public static boolean stringCompare(String a, String b) {
-        if (a == b) {
+        if (Objects.equals(a, b)) {
             return true;
         }
 
@@ -31,6 +35,40 @@ public class JavaTopics {
             temporaryString.append(providedString.charAt(i));
         }
         System.out.println("Reversed String: " + temporaryString);
+    }
+
+    public static void method1() {
+        System.out.println("Method without parameter");
+    }
+
+    //Method overloading. Method overloading is done with parameters only, having different return type don't qualify method for overloading
+    public static String method1(String str) {
+        System.out.println("Method with parameter but different return type");
+        return str;
+    }
+
+    public static void matrixInput() {
+        System.out.println("Provide 4 number input for 2D array");
+        int[][] twoDArray = new int[2][2];
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextInt()) {
+            for (int i = 0; i< twoDArray.length; i++) {
+                for(int j = 0; j < twoDArray.length; j++) {
+                    twoDArray[i][j] = scanner.nextInt();
+                }
+            }
+        } else {
+            System.out.println("Please provide number inputs only");
+            matrixInput();
+        }
+
+        for (int[] ints : twoDArray) {
+            for (int j = 0; j < twoDArray.length; j++) {
+                System.out.print(ints[j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
