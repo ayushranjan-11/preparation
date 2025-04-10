b4049bc8de330a60842a10893345025cd1d7cf2c
ayushranjan-11 <ayush.ranjan@gmail.com>
2025-04-11 02:04:40 +0530

    Explored few topics around set and string

--- src/test/java/InterviewQuestionsPractise/JavaTopics.java
+++ src/test/java/InterviewQuestionsPractise/JavaTopics.java
@@ -1,9 +1,6 @@
 package InterviewQuestionsPractise;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class JavaTopics {
     public static void main(String[] args) {
@@ -14,7 +11,8 @@ public class JavaTopics {
 //        stringReverse("Hello");
 //        matrixInput();
 //        System.out.println(stringReverseWithStringBuilder("Ayush"));
-        listTesting();
+//        listTesting();
+        vowelCheck("Ayush Ranjan");
     }
 
     public static void main(String string) {
@@ -26,11 +24,7 @@ public class JavaTopics {
     }
 
     public static boolean stringCompare(String a, String b) {
-        if (Objects.equals(a, b)) {
-            return true;
-        }
-
-        return false;
+        return Objects.equals(a, b);
     }
 
     public static void stringReverse(String providedString) {
@@ -55,9 +49,9 @@ public class JavaTopics {
         System.out.println("Provide 4 number input for 2D array");
         int[][] twoDArray = new int[2][2];
         Scanner scanner = new Scanner(System.in);
-        if(scanner.hasNextInt()) {
-            for (int i = 0; i< twoDArray.length; i++) {
-                for(int j = 0; j < twoDArray.length; j++) {
+        if (scanner.hasNextInt()) {
+            for (int i = 0; i < twoDArray.length; i++) {
+                for (int j = 0; j < twoDArray.length; j++) {
                     twoDArray[i][j] = scanner.nextInt();
                 }
             }
@@ -68,7 +62,7 @@ public class JavaTopics {
 
         for (int[] ints : twoDArray) {
             for (int j = 0; j < twoDArray.length; j++) {
-                System.out.print(ints[j]+" ");
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
@@ -79,18 +73,14 @@ public class JavaTopics {
         return new StringBuilder(stringToBeReversed).reverse().toString().toLowerCase();
     }
 
-    public void nonStaticMethod(){
-        System.out.println("Non static method for this class");
-    }
-
     public static void listTesting() {
         List<Integer> integerList = new ArrayList<>();
 
         //integerList.add(0,1);
         int listInput = 0;
 
-        for(int i = 0; i < 10; i++) {
-            integerList.add(i,listInput++);
+        for (int i = 0; i < 10; i++) {
+            integerList.add(i, listInput++);
         }
 
         for (Integer integer : integerList) {
@@ -98,4 +88,34 @@ public class JavaTopics {
         }
     }
 
+    public static String vowelCheck(String stringToCheck) {
+        StringBuilder vowelkeeper = new StringBuilder();
+        stringToCheck = stringToCheck.toLowerCase();
+
+        Map<Character, Integer> vowelCount = new LinkedHashMap<>();
+        vowelCount.put('a', 0);
+        vowelCount.put('e', 0);
+        vowelCount.put('i', 0);
+        vowelCount.put('o', 0);
+        vowelCount.put('u', 0);
+        for (int i = 0; i < stringToCheck.length(); i++) {
+            if (stringToCheck.charAt(i) == 'a' || stringToCheck.charAt(i) == 'e' || stringToCheck.charAt(i) == 'i'
+                    || stringToCheck.charAt(i) == 'o' || stringToCheck.charAt(i) == 'u') {
+
+                vowelkeeper.append(stringToCheck.charAt(i));
+                vowelCount.put(stringToCheck.charAt(i), vowelCount.get(stringToCheck.charAt(i))+1);
+
+                //TODO: Improve the duplicate addition of the characters
+            }
+        }
+        System.out.println("Present vowels were: " + vowelkeeper);
+        for(Map.Entry<Character, Integer> e: vowelCount.entrySet()) {
+            System.out.printf("  %c: %d%n", e.getKey(), e.getValue());
+        }
+        return vowelkeeper.toString();
+    }
+
+    public void nonStaticMethod() {
+        System.out.println("Non static method for this class");
+    }
 }
