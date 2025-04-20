441b8f24248b98f3c2161a021adb69b07a76e201
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-04-21 00:36:48 +0530

    Added browser pop up value entry code on selenium section

--- src/test/java/SeleniumCodes/LoginIntoPopUps.java
+++ src/test/java/SeleniumCodes/LoginIntoPopUps.java
@@ -4,23 +4,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
+
 public class LoginIntoPopUps {
-	//Demonstration for login when pop up is shown
-	String baseURL = "https://the-internet.herokuapp.com/";
-	WebDriver driver = new ChromeDriver();
-	
-	public static void main(String[] args) {
-		LoginIntoPopUps loginIntoPopUps = new LoginIntoPopUps();
-		loginIntoPopUps.browserOpenAndNavigate();
-	}
-	
-	
-	void browserOpenAndNavigate() {
-		driver.get(baseURL);
-	}
-	
-	void optionSelectAndPopUpOpen() {
-		String optionXpath = "//a[normalize-space()='Basic Auth']";
-		driver.findElement(By.xpath(optionXpath)).click();
-	}
+    //Demonstration for login when pop up is shown
+    String baseURL = "https://the-internet.herokuapp.com/";
+    String authURL = "https://the-internet.herokuapp.com/basic_auth";
+    String popUpUsernameAndPassword = "admin:admin@";
+    WebDriver driver = new ChromeDriver();
+
+    public static void main(String[] args) {
+        LoginIntoPopUps loginIntoPopUps = new LoginIntoPopUps();
+        loginIntoPopUps.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        loginIntoPopUps.browserOpenAndNavigate();
+        loginIntoPopUps.optionSelectAndPopUpOpen();
+        loginIntoPopUps.popUpValueEntry();
+
+        //loginIntoPopUps.popUpValueEntryWithSendKeys();
+    }
+
+
+    void browserOpenAndNavigate() {
+        driver.get(baseURL);
+    }
+
+    void optionSelectAndPopUpOpen() {
+        String optionXpath = "//a[normalize-space()='Basic Auth']";
+        driver.findElement(By.xpath(optionXpath)).click();
+    }
+
+    void popUpValueEntry() {
+
+        //This will be adding username and password in the url as append
+        String[] urls = driver.getCurrentUrl().split("//");
+        urls[0] += "//";
+        urls[1] = popUpUsernameAndPassword + urls[1];
+
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < urls.length; i++) {
+            stringBuffer.append(urls[i]);
+        }
+
+        driver.get(stringBuffer.toString());
+
+    }
+
+//    void popUpValueEntryWithSendKeys(){
+//        driver.switchTo().alert().sendKeys("admin");
+//
+//    }
 }
--- src/test/java/SeleniumCodes/WorldPopulationNumber.java
+++ src/test/java/SeleniumCodes/WorldPopulationNumber.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v132.page.model.Screenshot;
+//import org.openqa.selenium.devtools.v132.page.model.Screenshot;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
