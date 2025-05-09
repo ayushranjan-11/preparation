c8c187647a196c63f054fd5a84ce3d469208ef80
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-05-09 20:55:13 +0530

    Added evenOddCheck

--- src/test/java/InterviewQuestionsPractise/ReverseNumber.java
+++ src/test/java/InterviewQuestionsPractise/ReverseNumber.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class ReverseNumber {
     
     public static void main(String[] args) {
-        reverseGiveNummber(1234);
-        pallindromCheck("141");
-        pallindromCheck(141);
+        // reverseGiveNummber(1234);
+        // pallindromCheck("141");
+        // pallindromCheck(141);
+        evenOddCheck(24687610);
     }
 
     static void reverseGiveNummber(int givenNumber) {
@@ -42,14 +43,41 @@ public class ReverseNumber {
         int reversedNumberSave = 0, temp = 0;
         int numberToCheckCopy = numberToCheck;
 
-        while(numberToCheckCopy> 0) {
+        if(numberToCheck > 9) {
+            while(numberToCheckCopy> 0) {
             temp = numberToCheckCopy %10;
             reversedNumberSave = reversedNumberSave*10+temp;
             numberToCheckCopy = numberToCheckCopy / 10;
         }
+        } else System.out.println("Single digit number cannot be pallindrom");
+        
         System.out.println(reversedNumberSave);
 
         if(reversedNumberSave == numberToCheck) System.out.println("Number is Pallindrom");
         else System.out.println("Number is not pallindrom");
+    }
+
+    static void evenOddCheck(int numberToCheck) {
+        //Input = 24687610
+        //Output = Even: 5 Odd: 3
+        int numberToCheckCopy = numberToCheck;
+        int temp = 0, oddCount = 0, evenCount = 0;
+        if(numberToCheckCopy > 1) {
+            if(numberToCheckCopy > 9) {
+                while(numberToCheckCopy > 0) {
+                temp = numberToCheckCopy % 10;
+                if(temp % 2 == 0 && temp!= 0) {
+                    evenCount++;
+                } else oddCount ++;
+                numberToCheckCopy = numberToCheckCopy / 10;
+            }
+            } else {
+                if(numberToCheckCopy%2 == 0) {
+                    evenCount++;
+                } else oddCount++;
+            }
+            
+            System.out.println("Given number was: "+numberToCheck+" even count is: "+evenCount+" odd count is: "+oddCount);
+        } else System.out.println("Number should be greater than 1 to check even and odd");
     }
 }
